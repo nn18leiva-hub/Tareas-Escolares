98185d9a72645e748af1eb7a14c9fd4f8117dce0
Nelson Leiva <nn13leiva@gmail.com>
2026-03-11 11:26:58 -0500

    tatreas

--- backend/src/routes/materias.routes.js
+++ backend/src/routes/materias.routes.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const verificarToken = require('../middlewares/auth.middleware');
+const controller = require('../controllers/materias.controller');
 
+// Crear materia
+router.post('/', verificarToken, controller.crearMateria);
 
-// rutas se agregarán después
+// Listar todas las materias
+router.get('/', verificarToken, controller.obtenerTodasLasMaterias);
+
+// Listar materias por periodo
+router.get('/:id_periodo', verificarToken, controller.obtenerMateriasPorPeriodo);
+
+// Obtener detalle de una materia
+router.get('/detalle/:id', verificarToken, controller.obtenerMateriaPorId);
+
+// Actualizar materia
+router.put('/:id', verificarToken, controller.actualizarMateria);
+
+// Eliminar materia
+router.delete('/:id', verificarToken, controller.eliminarMateria);
 
 module.exports = router;
--- backend/src/routes/tareas.routes.js
+++ backend/src/routes/tareas.routes.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const verificarToken = require('../middlewares/auth.middleware');
+const controller = require('../controllers/tareas.controller');
+/*
+http://localhost:3000/api/tareas  metodo: POST nueva tarea
+http://localhost:3000/api/tareas  metodo: GET listar todas las tareas
+http://localhost:3000/api/tareas/id metodo: GET listar 1 tarea
+http://localhost:3000/api/tareas/id metodo:PUT actualizar tarea
+http://localhost:3000/api/tareas/id/completar metodo: PATCH cambiar estado
+*/
 
 
-// rutas se agregarán después
+//crear una nueva tarea
+router.post('/', verificarToken, controller.crearTarea);
+//consultar todas las tareas
+router.get('/', verificarToken, controller.obtenerTodasLasTareas);
+//consultar una tarea segun su id
+router.get('/:id', verificarToken, controller.obtenerTareaPorId);
+//actualizar un tarea
+router.put('/:id', verificarToken, controller.actualizarTarea);
+//marcar como completada una tarea
+router.patch('/:id/completar', verificarToken, controller.marcarComoCompletada);
+
+router.delete('/:id', verificarToken, controller.eliminarTarea);
+
+
+
+router.get('/estado/pendientes', verificarToken, controller.tareasPendientes);
+router.get('/estado/vencidas', verificarToken, controller.tareasVencidas);
+router.get('/estado/completadas', verificarToken, controller.tareasCompletadas);
+
 
 module.exports = router;
+
